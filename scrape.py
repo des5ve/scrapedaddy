3a4ba6678686b9c7bca92fd6bbda102c5d8f11e0
Unknown <des5ve@virginia.edu>
2017-09-14 14:14:41 -0400

    Initial Draft of scraper

---
+++ scrape.py
@@ -0,0 +1,11 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+url = 'http://www.espn.com/mlb/scoreboard'
+response = requests.get(url)
+html = response.content
+soup = BeautifulSoup(html, "html.parser")
+table = soup.find_all("article")
+for link in soup.find_all("article"):
+	print (link)
