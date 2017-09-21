9366978679debe4a5f06e9427c52bbc7109be2ed
adepu <adepu.shashank@gmail.com>
2017-09-21 10:21:18 -0400

    updated filtering

--- scrape.py
+++ scrape.py
@@ -6,6 +6,10 @@ url = 'http://www.espn.com/mlb/scoreboard'
 response = requests.get(url)
 html = response.content
 soup = BeautifulSoup(html, "html.parser")
+mainContainer = soup.find(id="main-container")
+scoreBoard = mainContainer.find_all(id="scoreboard-page")
+print(scoreBoard)
+
 #table = soup.find_all("article")
-for link in soup.find_all("section"):
-	print(link)
+# for link in soup.find_all("section"):
+# 	print(link)
