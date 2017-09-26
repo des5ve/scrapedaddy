ad340f6ed82423405a3de45d7bcae704aa6f785b
Unknown <des5ve@virginia.edu>
2017-09-26 09:13:56 -0400

    search on scores

--- scrape.py
+++ scrape.py
@@ -2,14 +2,10 @@ from bs4 import BeautifulSoup
 import requests
 
 
-url = 'http://www.espn.com/mlb/scoreboard'
+url = 'http://www.nfl.com/scores'
 response = requests.get(url)
 html = response.content
 soup = BeautifulSoup(html, "html.parser")
-mainContainer = soup.find(id="main-container")
-scoreBoard = mainContainer.find_all(id="scoreboard-page")
-print(scoreBoard)
-
-#table = soup.find_all("article")
-# for link in soup.find_all("section"):
-# 	print(link)
+mainContainer = soup.find(id="score-boxes")
+scores = mainContainer.find_all(class_ = "total-score")
+print (scores)
