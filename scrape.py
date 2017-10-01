213c85c908324e402f65fe8d7b38c016b3ce8dc3
adepu <adepu.shashank@gmail.com>
2017-10-01 09:22:10 -0400

    getting home team and away team scores

--- scrape.py
+++ scrape.py
@@ -2,12 +2,18 @@ from bs4 import BeautifulSoup
 import requests
 
 
-url = 'http://www.nfl.com/scores'
+url = 'http://www.nfl.com/scores/2017/REG3'
 response = requests.get(url)
 html = response.content
 soup = BeautifulSoup(html, "html.parser")
 mainContainer = soup.find(id="score-boxes")
-scores = mainContainer.find_all(class_ = "team-data")
-print (scores)
-#for i in scores.find_all("p"):
- #   print (i)
+#mainContainer = soup.find(class_="score-boxes")
+scoreBoard = mainContainer.find(class_="scorebox-wrapper")
+awayTeam = scoreBoard.find(class_="away-team")
+homeTeam = scoreBoard.find(class_="home-team")
+awayTeamScore = awayTeam.find(class_="total-score").get_text()
+homeTeamScore = homeTeam.find(class_="total-score").get_text()
+awayTeamName = awayTeam.find(class_="team-name").find('a').get_text()
+homeTeamName = homeTeam.find(class_="team-name").find('a').get_text()
+print("Home Team: "+homeTeamName+", Score: "+homeTeamScore)
+print("Away Team: "+awayTeamName+", Score: "+awayTeamScore)
