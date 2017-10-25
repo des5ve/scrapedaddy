1e047f5973fa87ffeb423dee0273e41c3a7af6eb
Unknown <des5ve@virginia.edu>
2017-10-25 15:44:34 -0400

    Auto stash before merge of "master" and "origin/master"
    attempting to insert some shit

--- scrapebaseball.py
+++ scrapebaseball.py
@@ -3,15 +3,10 @@ import pandas as pd
 import requests
 
 
-url = 'https://www.mlb.com/scores'
+url = 'http://www.foxsports.com/mlb/scores?season=2017&date=2017-10-06'
 response = requests.get(url)
 html = response.content
 soup = BeautifulSoup(html, "html.parser")
 
-mainContainer = soup.find(id = "scores_index")
-scoreboard = mainContainer.find(class_ = "l-grid__content l-grid__content--none")
-scores = scoreboard.find(class_ = "mlb-scores mlb-scores--responsive mlb-scores--desktop")
-
-mainScoresContainer = soup.find(class_ = "mlb-scores")
-
-print (mainScoresContainer)
+mainContainer = soup.find(class_ ="wisbb_body")
+print (mainContainer)
