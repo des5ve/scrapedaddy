1f5ad5e7bdac2e8d481e93afdc73bbc92135e5dc
Unknown <des5ve@virginia.edu>
2017-11-09 15:40:21 -0500

    got it workin now

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   moduleId: module.id,
-  templateUrl: 'component.html'
+  templateUrl: 'app.component.html'
 })
 export class AppComponent  { name = 'Angular'; }
