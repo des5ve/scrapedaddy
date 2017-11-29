75cdc9d7fce19ee3bb884e1b94afa7360d59017a
Unknown <des5ve@virginia.edu>
2017-11-28 20:58:26 -0500

    start of scoreboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   moduleId: module.id,
-  templateUrl: 'app.component.html'
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
 })
 export class AppComponent  { 
 //[] means property binding - one way up C to D
