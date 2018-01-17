c921a9a9f10005a692845a9096d9a7cf9b93eb74
Shashank Adepu <adepu.shashank@gmail.com>
2018-01-16 23:04:02 -0500

    betting modal component

---
+++ ui/new-app/src/app/betting-modal/betting-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-betting-modal',
+  templateUrl: './betting-modal.component.html',
+  styleUrls: ['./betting-modal.component.css']
+})
+export class BettingModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
