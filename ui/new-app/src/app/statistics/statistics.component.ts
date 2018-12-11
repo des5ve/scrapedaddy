1fb81a16e967c4f23a6144793f1b2c5856f427a9
adepu <adepu.shashank@gmail.com>
2018-12-11 00:53:27 -0500

    charts showing, fixed twitter and scoreboard formatting

--- ui/new-app/src/app/statistics/statistics.component.ts
+++ ui/new-app/src/app/statistics/statistics.component.ts
@@ -1,42 +1,65 @@
-import { Component, OnInit } from '@angular/core';
-import { NflTeamStat } from '../models/NflTeamStat'
+import { Component } from '@angular/core';
+import { NflTeamStat } from '../models/NflTeamStat';
 
 @Component({
   selector: 'app-statistics',
   templateUrl: './statistics.component.html',
   styleUrls: ['./statistics.component.css']
 })
-export class StatisticsComponent implements OnInit {
+export class StatisticsComponent {
 
   teamName: string = "Butt";
   nflTeamStat: NflTeamStat[] = [];
   
   
-   // events
-   public chartClicked(e:any):void {
-     console.log(e);
-   }
+  // Radar
+  public radarChartLabels:string[] = ['Eating', 'Drinking', 'Sleeping', 'Designing', 'Coding', 'Cycling', 'Running'];
+
+  public radarChartData:any = [
+    {data: [Math.floor((Math.random() * 100) + 1), 
+            Math.floor((Math.random() * 100) + 1), 
+            Math.floor((Math.random() * 100) + 1), 
+            Math.floor((Math.random() * 100) + 1),
+            Math.floor((Math.random() * 100) + 1), 
+            Math.floor((Math.random() * 100) + 1),
+            Math.floor((Math.random() * 100) + 1)], label: 'Series A'},
+    {data: [Math.floor((Math.random() * 100) + 1), 
+            Math.floor((Math.random() * 100) + 1), 
+            Math.floor((Math.random() * 100) + 1), 
+            Math.floor((Math.random() * 100) + 1),
+            Math.floor((Math.random() * 100) + 1), 
+            Math.floor((Math.random() * 100) + 1),
+            Math.floor((Math.random() * 100) + 1)], label: 'Series B'}
+  ];
+
+  public radarChartType:string = 'radar';
+
+  // events
+  public chartClicked(e:any):void {
+    console.log(e);
+  }
   
-   public chartHovered(e:any):void {
-     console.log(e);
-   }
+  public chartHovered(e:any):void {
+    console.log(e);
+  }
 
   constructor() { }
 
-  switchTeam(value: string){
+  switchTeam(value: string) {
+    const barChartOptions = {
+      title: {
+        text: value,
+        display: true
+      }
+    };
+
     this.teamName = value;
     const newNflTeamStat = new NflTeamStat();
     newNflTeamStat.radarChartLabels = ['Eating', 'Drinking', 'Sleeping', 'Designing', 'Coding', 'Cycling', 'Running'];
     newNflTeamStat.radarChartData = {data: [65, 59, 90, 81, 56, 55, 40], label: 'Series A'};
     newNflTeamStat.radarChartType = 'radar';
-    
     this.nflTeamStat.push(newNflTeamStat);
     console.log(newNflTeamStat);
-  };
-
-  ngOnInit() {
-
-    
+    console.log(this.nflTeamStat);
   }
-
 }
